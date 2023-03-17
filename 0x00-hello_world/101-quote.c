c4c2fd7c61af45a6d0d2ef216e7f718d3fbbabfa
habeebah23 <habeebah809@gmail.com>
2023-03-16 19:50:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - A c program that prints a line to the standard error
  * Return i (Sucess)
- * \
+ *\
 int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stoudt);
